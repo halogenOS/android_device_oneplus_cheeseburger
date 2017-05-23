4ea1990a04c2fb89deecb38e3d2cddf3603c7b55
Simao Gomes Viana <xdevs23@outlook.com>
2017-05-23 19:58:10 +0200

    Get this further
    Change-Id: I0a2e2f43108cbc6232407184c16ff30ff2d38f04
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- device.mk
+++ device.mk
@@ -1,4 +1,6 @@
-DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8998/overlay
+PLATFORM_PATH := device/oneplus/oneplus5
+
+DEVICE_PACKAGE_OVERLAYS := $(PLATFORM_PATH)/overlay
 TARGET_KERNEL_VERSION := 4.4
 BOARD_HAVE_QCOM_FM := true
 TARGET_USES_NQ_NFC := true
@@ -16,12 +18,12 @@ TARGET_USE_UI_SVA := true
 
 # Video codec configuration files
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
-PRODUCT_COPY_FILES += device/qcom/msm8998/media_profiles.xml:system/etc/media_profiles.xml \
-                      device/qcom/msm8998/media_codecs.xml:system/etc/media_codecs.xml \
-                      device/qcom/msm8998/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+PRODUCT_COPY_FILES += $(PLATFORM_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+                      $(PLATFORM_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+                      $(PLATFORM_PATH)/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
-PRODUCT_COPY_FILES += device/qcom/msm8998/whitelistedapps.xml:system/vendor/etc/whitelistedapps.xml
+PRODUCT_COPY_FILES += $(PLATFORM_PATH)/whitelistedapps.xml:system/vendor/etc/whitelistedapps.xml
 
 #QTIC flag
 -include $(QCPATH)/common/config/qtic-config.mk
@@ -29,13 +31,8 @@ PRODUCT_COPY_FILES += device/qcom/msm8998/whitelistedapps.xml:system/vendor/etc/
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, device/qcom/common/common64.mk)
-
-PRODUCT_NAME := msm8998
-PRODUCT_DEVICE := msm8998
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := MSM8998 for arm64
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, device/qcom/common/common.mk)
 
 # Enable features in video HAL that can compile only on this platform
 TARGET_USES_MEDIA_EXTENSIONS := true
@@ -60,9 +57,6 @@ endif
 ifeq ($(strip $(BOARD_HAVE_QCOM_FM)),true)
 PRODUCT_BOOT_JARS += qcom.fmradio
 endif #BOARD_HAVE_QCOM_FM
-
-# Audio configuration file
--include $(TOPDIR)hardware/qcom/audio/configs/msm8998/msm8998.mk
 
 PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
         $(QCPATH)/qrdplus/globalization/multi-language/res-overlay \
@@ -117,14 +111,14 @@ PRODUCT_COPY_FILES += \
 
 # FBE support
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8998/init.qti.qseecomd.sh:system/bin/init.qti.qseecomd.sh
+    $(PLATFORM_PATH)/init.qti.qseecomd.sh:system/bin/init.qti.qseecomd.sh
 
 # MSM IRQ Balancer configuration file
-PRODUCT_COPY_FILES += device/qcom/msm8998/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+PRODUCT_COPY_FILES += $(PLATFORM_PATH)/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
-device/qcom/msm8998/powerhint_soc_id_292.xml:system/etc/powerhint_soc_id_292.xml
+    $(PLATFORM_PATH)/powerhint_soc_id_292.xml:system/etc/powerhint_soc_id_292.xml
 
 #for android_filesystem_config.h
 PRODUCT_PACKAGES += \
