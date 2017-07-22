mkodekar/android_device_oneplus_oneplus2-2

Kang vr hal from angler

device.mk
@@ -264,6 +264,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
+# VR
+PRODUCT_PACKAGES += \
+    vr.msm8994
+
 # MIDI feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml

halogenOS/android_device_oneplus_cheeseburger

Change-Id: I2fb813f3f1068f7506b68cb20dbdf79b862c6a85
Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

device.mk
@@ -276,6 +276,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvehiclenetwork-native
 
+# VR
+PRODUCT_PACKAGES += \
+    vr.msm8998
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \