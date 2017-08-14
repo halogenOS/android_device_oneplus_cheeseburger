0dbfa0d199857312e2e5b9e4911b9b8d378e4e28
shoudil <stephenli@codeaurora.org>
2017-08-14 18:04:55 +0800

    msm8998: adjust hwui param for 2k panel
    Adjust hwui param "ro.hwui.text_large_cache_height" to 4096
    to reduce janks for specific case (wechat emoji scroll).
    
    Change-Id: I6ef6f8aa70ba990d92625367bd4606bc49582e37
    CRs-Fixed:2085506

--- device.mk
+++ device.mk
@@ -76,6 +76,15 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=25
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=8m
+
 # HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=96 \
@@ -88,16 +97,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_small_cache_width=2048 \
     ro.hwui.text_small_cache_height=2048 \
     ro.hwui.text_large_cache_width=3072 \
-    ro.hwui.text_large_cache_height=2048
-
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=8m
+    ro.hwui.text_large_cache_height=4096
 
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
