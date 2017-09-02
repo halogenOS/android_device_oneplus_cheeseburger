4e4b4cc0ca8cd2061cb90bc2ab12e40fdd3987f9
Simao Gomes Viana <xdevs23@outlook.com>
2017-09-02 20:04:55 +0200

    Bye audio hal
    Change-Id: I74395f8fa9fd91cee3f62694d85c7f7900b40a6c

--- audio/msm8998.mk
+++ audio/msm8998.mk
@@ -65,36 +65,36 @@ AUDIO_FEATURE_ENABLED_RAS := true
 DEVICE_PACKAGE_OVERLAYS += device/oneplus/cheeseburger/audio/configs/common/overlay
 
 PRODUCT_COPY_FILES += \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_policy.conf:system/etc/audio_policy.conf \
-    device/oneplus/cheeseburger/audio/configs/msm8998/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/mixer_paths_qvr.xml:system/etc/mixer_paths_qvr.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/mixer_paths_tavil.xml:system/etc/mixer_paths_tavil.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/mixer_paths_dtp.xml:system/etc/mixer_paths_dtp.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    device/oneplus/cheeseburger/audio/configs/msm8998/aanc_tuning_mixer_tavil.txt:system/etc/aanc_tuning_mixer_tavil.txt \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/sound_trigger_mixer_paths_wcd9340.xml:system/etc/sound_trigger_mixer_paths_wcd9340.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/graphite_ipc_platform_info.xml:system/etc/graphite_ipc_platform_info.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
-    device/oneplus/cheeseburger/audio/configs/msm8998/default_volume_tables.xml:system/etc/default_volume_tables.xml
+    device/oneplus/cheeseburger/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    device/oneplus/cheeseburger/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    device/oneplus/cheeseburger/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    device/oneplus/cheeseburger/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/oneplus/cheeseburger/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml \
+    device/oneplus/cheeseburger/audio/mixer_paths_qvr.xml:system/etc/mixer_paths_qvr.xml \
+    device/oneplus/cheeseburger/audio/mixer_paths_tavil.xml:system/etc/mixer_paths_tavil.xml \
+    device/oneplus/cheeseburger/audio/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
+    device/oneplus/cheeseburger/audio/mixer_paths_dtp.xml:system/etc/mixer_paths_dtp.xml \
+    device/oneplus/cheeseburger/audio/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
+    device/oneplus/cheeseburger/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
+    device/oneplus/cheeseburger/audio/aanc_tuning_mixer_tavil.txt:system/etc/aanc_tuning_mixer_tavil.txt \
+    device/oneplus/cheeseburger/audio/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
+    device/oneplus/cheeseburger/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    device/oneplus/cheeseburger/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
+    device/oneplus/cheeseburger/audio/sound_trigger_mixer_paths_wcd9340.xml:system/etc/sound_trigger_mixer_paths_wcd9340.xml \
+    device/oneplus/cheeseburger/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    device/oneplus/cheeseburger/audio/graphite_ipc_platform_info.xml:system/etc/graphite_ipc_platform_info.xml \
+    device/oneplus/cheeseburger/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    device/oneplus/cheeseburger/audio/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
+    device/oneplus/cheeseburger/audio/default_volume_tables.xml:system/etc/default_volume_tables.xml
 
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    device/oneplus/cheeseburger/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 else
 PRODUCT_COPY_FILES += \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    device/oneplus/cheeseburger/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
@@ -104,4 +104,4 @@ endif
 
 # Listen configuration file
 PRODUCT_COPY_FILES += \
-    device/oneplus/cheeseburger/audio/configs/msm8998/listen_platform_info.xml:system/etc/listen_platform_info.xml
+    device/oneplus/cheeseburger/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml
