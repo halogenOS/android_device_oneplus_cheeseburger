a6e254cb5e614a44d7dfecea275c7afdab84f874
Alexander Martinz <alex@amartinz.at>
2017-09-08 23:23:46 +0200

    cheeseburger: fingerprint: build service instead of impl
    Change-Id: I355ae4899c36009578ca53dc276e0059ff7a7c5e
    Signed-off-by: Alexander Martinz <alex@amartinz.at>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- device.mk
+++ device.mk
@@ -188,9 +188,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-impl \
+    android.hardware.biometrics.fingerprint@2.1-service \
     fingerprintd \
-    android.hardware.biometrics.fingerprint@2.1-service
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
