eaa6fa3acf35b6cc92a9a561a384425d0298a2e3
Alexander Martinz <alex@amartinz.at>
2017-09-08 23:23:46 +0200

    Fingerprint: build service instead of impl
    Change-Id: I355ae4899c36009578ca53dc276e0059ff7a7c5e
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- device.mk
+++ device.mk
@@ -192,7 +192,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-impl \
+    android.hardware.biometrics.fingerprint@2.1-service \
     fingerprintd
 
 # For android_filesystem_config.h
