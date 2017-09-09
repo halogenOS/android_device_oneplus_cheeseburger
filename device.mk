e74fb38425c36c4702e1b464ccc8f912799e6630
Alexander Martinz <alex@amartinz.at>
2017-09-09 17:57:23 +0200

    Add drm to manifest, build impl
    Change-Id: I878da134ba3a3876c7d0cfdebacae75fe5caf4e5
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- device.mk
+++ device.mk
@@ -184,7 +184,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
