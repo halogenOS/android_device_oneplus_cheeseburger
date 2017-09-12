4cecbf30807e0098278235d21b6bb5cc7d15c5a4
Marko Man <darkobas@gmail.com>
2017-09-12 21:33:35 +0200

    Fix gps
    Change-Id: Ic0505bdd553cff9fbcd8cf3d7cc71fb8410cd4f3

--- device.mk
+++ device.mk
@@ -205,7 +205,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0 \
+    android.hardware.gnss@1.0-impl \
     gps.msm8998 \
     gps.conf \
     libgps.utils \
