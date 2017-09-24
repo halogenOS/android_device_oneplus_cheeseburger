8621c4be7d2bb225d291aa44f95d4b11e9c14631
Marko Man <darkobas@gmail.com>
2017-09-24 15:41:36 -0400

    cheeseburger: enable camera shim
    Change-Id: Id142122361f0d37d85547db1d6e5a54759c74e74
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- device.mk
+++ device.mk
@@ -132,11 +132,21 @@ PRODUCT_PACKAGES += \
     libbt-vendor \
     android.hardware.bluetooth@1.0-impl
 
+# Camera configuration file. Shared by passthrough/binderized camera HAL
+PRODUCT_PACKAGES += camera.device@3.2-impl
+#PRODUCT_PACKAGES += camera.device@1.0-impl
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+# Enable binderized camera HAL
+#PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    Snap
+    libcamera_shim \
+    libcamera_shim_display \
+    libcamera_shim_buffer \
+    libcamera_shim_native
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
