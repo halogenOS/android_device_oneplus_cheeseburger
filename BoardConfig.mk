e3ecd449df0ecbe99d80b2bed4f55139f4a9a2cb
Simao Gomes Viana <xdevs23@outlook.com>
2017-10-18 20:58:37 +0200

    BoardConfig: simplify dexpreopt
    Change-Id: I4ef80ead57205f7f455fce7033c37f6f1a2db325

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,14 +183,7 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-   endif
-  endif
-endif
+WITH_DEXPREOPT ?= false
 
 # Filesystem
 TARGET_FS_CONFIG_GEN += $(PLATFORM_PATH)/config.fs
