5f3246d30bf4d7dcc75682d05e36da649b0df456
Simao Gomes Viana <xdevs23@outlook.com>
2017-11-01 22:26:31 +0100

    Revert "Fix sensors"
    This reverts commit 078cab393ed3d6872bf7220288103a3600085b9b.
    
    Change-Id: Ie903b9b6d9f2a012d9cde4c96dc8f1d1b6c568c2

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -77,7 +77,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Thermal
 PRODUCT_PACKAGES += \
