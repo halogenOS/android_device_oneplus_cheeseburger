795e1c1ff752c8696b13f6fd47d2e2c898027db9
Simao Gomes Viana <xdevs23@outlook.com>
2018-01-03 13:21:37 +0100

    Eleph extract scripts from op2 device tree
    Change-Id: I560abae6d1f29789c566ce4d9efc489f4af9514a

---
+++ extract-files.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=cheeseburger
+VENDOR=oneplus
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+XOS_ROOT="$MY_DIR"/../../..
+
+HELPER="$XOS_ROOT"/vendor/xos/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 1 ]; then
+  SRC=$1
+else
+  echo "$0: bad number of arguments"
+  echo ""
+  echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+  echo ""
+  echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+  echo "the device using adb pull."
+  exit 1
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$XOS_ROOT"
+
+#extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
+#extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC"
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh
+
+#CAMERA_HAL="$XOS_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/hw/camera.msm8996.so
+
+#sed -i \
+#    -e 's/_ZN7qcamera17QCameraParameters16setQuadraCfaModeEjb/_ZN7qcamera17QCameraParameters16setQuadraCfaModSHIM/' \
+#    -e 's/_ZN7qcamera17QCameraParameters12setQuadraCfaERKS0_/_ZN7qcamera17QCameraParameters12setQuadraCfaERSHIM/' \
+#    -e 's/_ZN7qcamera17QCameraParameters12getQuadraCfaEv/_ZN7qcamera17QCameraParameters12getQuadraCSHIM/' \
+#    -e 's/_ZN7qcamera15isOneplusCameraEv/_ZN7qcamera15isOneplusCameSHIM/' \
+#    -e 's/_ZN7qcamera17QCameraParameters15is3p8spLowLightEv/_ZN7qcamera17QCameraParameters15is3p8spLowLigSHIM/' \
+#    -e 's/_ZN7qcamera17QCameraParameters21handleSuperResoultionEv/_ZN7qcamera17QCameraParameters21handleSuperResoultiSHIM/' \
+#    -e 's/_ZN7qcamera17QCameraParameters17isSuperResoultionEv/_ZN7qcamera17QCameraParameters17isSuperResoultiSHIM/' \
+#    "$CAMERA_HAL"
